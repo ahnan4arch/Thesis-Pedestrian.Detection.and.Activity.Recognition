2853f2e37735a994e373f0a8d6b37af487757ce8
OnceMore2020 <guanhwang2011@gmail.com>
2014-04-14 14:27:30 +0800

    revise

---
+++ Implementation/Haar/CMakeFiles/HaarCascade.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/HaarCascade.dir/haar.cpp.o"
+  "HaarCascade.pdb"
+  "HaarCascade"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/HaarCascade.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
