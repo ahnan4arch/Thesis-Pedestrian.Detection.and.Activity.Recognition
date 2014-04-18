6c2ca2219ad076408f8fa3f5332f466642c95575
OnceMore2020 <guanhwang2011@gmail.com>
2014-04-18 17:22:31 +0800

    revise

---
+++ Implementation/HOG/CMakeFiles/HOGDetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/HOGDetect.dir/peopledetect.cpp.o"
+  "HOGDetect.pdb"
+  "HOGDetect"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/HOGDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
