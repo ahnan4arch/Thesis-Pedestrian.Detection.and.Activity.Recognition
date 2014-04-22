88483cf050384f579df5d49bde93856f3bd2b2ce
OnceMore2020 <guanhwang2011@gmail.com>
2014-04-22 12:13:52 +0800

    delete unused files

--- Implementation/HOG/TrainHOG/svmlight/svm_common.o.d
+++
@@ -1,6 +0,0 @@
-svmlight/svm_common.o: svmlight/svm_common.c svmlight/svm_common.h \
- svmlight/kernel.h
-
-svmlight/svm_common.h:
-
-svmlight/kernel.h:
--- Implementation/HOG/TrainHOG/svmlight/svm_hideo.o.d
+++
@@ -1,3 +0,0 @@
-svmlight/svm_hideo.o: svmlight/svm_hideo.c svmlight/svm_common.h
-
-svmlight/svm_common.h:
